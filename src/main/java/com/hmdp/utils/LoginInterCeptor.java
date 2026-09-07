477bf22c8534969b8766fc1a96cba4cb78e1cd41
lilili111111 <15513126655@139.com>
2026-09-07 17:34:34 +0800

    实现校验登录拦截器

---
+++ src/main/java/com/hmdp/utils/LoginInterCeptor.java
@@ -0,0 +1,35 @@
+package com.hmdp.utils;
+
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.xml.ws.handler.Handler;
+
+public class LoginInterCeptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //获取session
+        HttpSession session = request.getSession();
+        //获取session中的用户
+        Object user = session.getAttribute("user");
+        //判断用户是否存在
+        //不存在，拦截
+       if (user==null){
+           response.setStatus(401);
+           return false;
+       }
+        //存在，保护用户ID到threadlocal
+UserHolder.saveUser((User) user);
+        //放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
