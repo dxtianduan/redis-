7db7b13cda096d62552739b022fdfa1d7b97df48
lilili111111 <15513126655@139.com>
2026-09-14 21:40:22 +0800

    解决Redis分布式误删的问题

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,6 +1,7 @@
 package com.hmdp.utils;
 
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.validation.Valid;
@@ -9,6 +10,8 @@ import java.util.concurrent.TimeUnit;
 public class SimpleRedisLock implements lLock{
    private String name;
    private final static  String key_="lock:";
+   private static final String id_=UUID.randomUUID().toString(true)+"-";
+
 private StringRedisTemplate stringRedisTemplate;
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
@@ -19,17 +22,23 @@ private StringRedisTemplate stringRedisTemplate;
     @Override
     public boolean tryLock(long timeoutSec) {
         //获取当前线程ID
-        long id = Thread.currentThread().getId();
+        String id =id_+ Thread.currentThread().getId();
         //获取锁
         Boolean success = stringRedisTemplate
                 .opsForValue()
-                .setIfAbsent(key_ + name, id + "", timeoutSec, TimeUnit.SECONDS);
+                .setIfAbsent(key_ + name, id, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unlock() {
+        //获取线程标志
+String threadID=id_+Thread.currentThread().getId();
+        //获取锁中的标志
+        String string = stringRedisTemplate.opsForValue().get(key_ + name);
+        //判断是否一致
+if (threadID.equals(string)){
         //释放锁
-stringRedisTemplate.delete(key_+name);
+stringRedisTemplate.delete(key_+name);}
     }
 }
